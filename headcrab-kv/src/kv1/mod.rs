0282f056e1f7f9177ebe49ba33a8e0104f0ea178
xrgqlo <xrgqlo21@gmail.com>
2026-03-09 22:55:55 +0100

    added unquoted key/values to kv1

--- headcrab-kv/src/kv1/mod.rs
+++ headcrab-kv/src/kv1/mod.rs
@@ -29,44 +29,41 @@ impl FromStr for KV1Tree {
         let mut lexer = KV1Token::lexer(s);
         let mut current_block: Vec<Block> = vec![];
         let mut depth = 0;
-        loop {
-            if let Some(token) = lexer.next() {
-                if token.is_err() {
-                    // needs proper error handling but whatever
-                    break;
-                }
 
-                let token = token.unwrap();
-                match token {
-                    KV1Token::Block => current_block.push(Block {
-                        name: lexer.slice().to_string(),
-                        blocks: vec![],
-                        keys: vec![],
-                    }),
-                    KV1Token::Pair => {
-                        let mut split = lexer.slice().split("\"");
-                        split.next();
-                        let name = split.next().unwrap();
-                        split.next();
-                        let value = split.next().unwrap();
-                        current_block[depth - 1]
-                            .keys
-                            .push(Key(name.to_string(), value.to_string()));
-                    }
-                    KV1Token::LeftBrace => depth += 1,
-                    KV1Token::RightBrace => {
-                        depth -= 1;
-                        if depth > 0 {
-                            let current = current_block[depth].clone();
-                            current_block[depth - 1].blocks.push(current);
-                        } else {
-                            blocks.push(current_block[depth].clone());
-                            current_block = vec![]
-                        }
+        while let Some(token) = lexer.next() {
+            if token.is_err() {
+                // needs proper error handling but whatever
+                break;
+            }
+
+            let token = token.unwrap();
+            match token {
+                KV1Token::Block => current_block.push(Block {
+                    name: lexer.slice().to_string(),
+                    blocks: vec![],
+                    keys: vec![],
+                }),
+                KV1Token::Pair => {
+                    let mut split = lexer.slice().split("\"");
+                    split.next();
+                    let name = split.next().unwrap();
+                    split.next();
+                    let value = split.next().unwrap();
+                    current_block[depth - 1]
+                        .keys
+                        .push(Key(name.to_string(), value.to_string()));
+                }
+                KV1Token::LeftBrace => depth += 1,
+                KV1Token::RightBrace => {
+                    depth -= 1;
+                    if depth > 0 {
+                        let current = current_block[depth].clone();
+                        current_block[depth - 1].blocks.push(current);
+                    } else {
+                        blocks.push(current_block[depth].clone());
+                        current_block = vec![]
                     }
                 }
-            } else {
-                break;
             }
         }
 
--- headcrab-kv/src/kv1/tokens.rs
+++ headcrab-kv/src/kv1/tokens.rs
@@ -2,10 +2,12 @@ use logos::Logos;
 
 #[derive(Debug, Logos, PartialEq)]
 #[logos(skip "[[:space:]]+")]
+#[logos(subpattern quoted = r#"\"(?:[^\\\"]|\\.)+\""#)]
+#[logos(subpattern non_quoted = r#"[^\"\s{}]+"#)]
 pub enum KV1Token {
     #[regex("[[:alnum:]]+")]
     Block,
-    #[regex("\"[^\"]+\"[ \t]+\"[^\"]+\"")]
+    #[regex("(?:(?&quoted)|(?&non_quoted))[ \t]+(?:(?&quoted)|(?&non_quoted))")]
     Pair,
     #[token("{")]
     LeftBrace,
