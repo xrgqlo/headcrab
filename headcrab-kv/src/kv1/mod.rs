2654f7b335951d10e8b63afc35e103b4ec317e2b
xrgqlo <xrgqlo21@gmail.com>
2026-03-10 13:23:16 +0100

    kv1: added parsing from path and basic error handling

--- headcrab-kv/src/kv1/mod.rs
+++ headcrab-kv/src/kv1/mod.rs
@@ -1,6 +1,11 @@
-use std::str::FromStr;
+use std::{
+    fs,
+    path::{Path, PathBuf},
+    str::FromStr,
+};
 
 use logos::Logos;
+use thiserror::Error;
 
 mod block;
 mod key;
@@ -11,32 +16,46 @@ pub use key::*;
 
 use tokens::*;
 
+pub type Result<T> = std::result::Result<T, ParsingError>;
+
 #[allow(dead_code)]
 #[derive(Debug, Eq, PartialEq)]
 pub struct KV1Tree {
     pub blocks: Vec<Block>,
 }
 
-#[allow(dead_code)]
-#[derive(Debug, PartialEq)]
-pub struct ParsingError;
+impl KV1Tree {
+    pub fn parse_from_file(path: impl AsRef<Path>) -> Result<Self> {
+        path.as_ref().try_into()
+    }
+}
+
+impl TryFrom<&Path> for KV1Tree {
+    type Error = ParsingError;
+
+    fn try_from(value: &Path) -> Result<Self> {
+        let content =
+            fs::read_to_string(value).map_err(|e| ParsingError::new(value.to_owned(), e.into()))?;
+        KV1Tree::from_str(&content).map_err(|e| ParsingError::new(value.to_owned(), e.into()))
+    }
+}
 
 impl FromStr for KV1Tree {
-    type Err = ParsingError;
+    type Err = LexerError;
 
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
+    fn from_str(s: &str) -> std::result::Result<Self, LexerError> {
         let mut blocks: Vec<Block> = vec![];
         let mut lexer = KV1Token::lexer(s);
         let mut current_block: Vec<Block> = vec![];
         let mut depth = 0;
 
-        while let Some(token) = lexer.next() {
-            if token.is_err() {
-                // needs proper error handling but whatever
-                break;
-            }
+        while let Some(token) = lexer
+            .next()
+            .transpose()
+            .map_err(|_| LexerError::Placeholder)?
+        {
+            // needs proper error handling but whatever
 
-            let token = token.unwrap();
             match token {
                 KV1Token::Block => current_block.push(Block {
                     name: lexer.slice().to_string(),
@@ -83,4 +102,34 @@ impl ToString for KV1Tree {
 
         string
     }
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Error)]
+#[error("could not parse {p}: {kind}")]
+pub struct ParsingError {
+    p: PathBuf,
+    kind: ParsingErrorKind,
+}
+
+impl ParsingError {
+    fn new(p: PathBuf, kind: ParsingErrorKind) -> Self {
+        Self { p, kind }
+    }
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Error)]
+pub enum ParsingErrorKind {
+    #[error("{0}")]
+    Io(#[from] std::io::Error),
+    #[error("{0}")]
+    Lexer(#[from] LexerError),
+}
+
+#[allow(dead_code)]
+#[derive(Debug, Error)]
+pub enum LexerError {
+    #[error("placeholder lexing error")]
+    Placeholder,
 }
